794fb1a79a7e83b8b5b4e87cabe4d4a0edb07cb8
amirthenew <ah.abshari@hotmail.com>
2023-09-13 02:02:45 +0430

    header creted

---
+++ webidemy/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Header.module.css'
+
+const Header = () => {
+    return (
+        <div>
+        <h1>webidemy</h1>
+        </div>
+      );
+}
+ 
+export default Header;
+<div>
+<h1>webidemy</h1>
+</div>
